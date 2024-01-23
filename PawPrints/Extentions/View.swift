e249f4e50312251260d7e08fea8916da705bf3c0
tchamalsha <tharu.chamalsha@gmail.com>
2024-01-23 11:46:04 +0530

    add login page

--- PawPrints/Extentions/View.swift
+++ PawPrints/Extentions/View.swift
@@ -1,32 +1,55 @@
 //
-//  View.swift
-//  PawPrints
+//  View+Extensions.swift
+//  SocialMedia
 //
-//  Created by Tharushi Chamalsha on 2023-11-24.
+//  Created by Balaji on 14/12/22.
 //
 
-import Foundation
 import SwiftUI
 
+// MARK: View Extensions For UI Building
 extension View{
+    // Closing All Active Keyboards
+    func closeKeyboard(){
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+    // MARK: Disabling with Opacity
+    func disableWithOpacity(_ condition: Bool)->some View{
+        self
+            .disabled(condition)
+            .opacity(condition ? 0.6 : 1)
+    }
+    
     func hAlign(_ alignment: Alignment)->some View{
         self
             .frame(maxWidth: .infinity,alignment: alignment)
     }
+    
     func vAlign(_ alignment: Alignment)->some View{
         self
             .frame(maxHeight: .infinity,alignment: alignment)
     }
     
-    //MARK: Custom Border View With padding
-    func border(_ width:CGFloat,_ color:Color)->some View{
+    // MARK: Custom Border View With Padding
+    func border(_ width: CGFloat,_ color: Color)->some View{
         self
             .padding(.horizontal,15)
             .padding(.vertical,10)
-            .background{
-                RoundedRectangle(cornerRadius: 5,style: .continuous)
-                    .stroke(color,lineWidth: width)
+            .background {
+                RoundedRectangle(cornerRadius: 5, style: .continuous)
+                    .stroke(color, lineWidth: width)
             }
-            .frame(width: 300,height: 50)
+    }
+    
+    // MARK: Custom Fill View With Padding
+    func fillView(_ color: Color)->some View{
+        self
+            .padding(.horizontal,15)
+            .padding(.vertical,10)
+            .background {
+                RoundedRectangle(cornerRadius: 5, style: .continuous)
+                    .fill(color)
+            }
     }
 }
---
+++ PawPrints/View/Login/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  PawPrints
+//
+//  Created by Tharushi Chamalsha on 2024-01-23.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
